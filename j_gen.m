9bdc0d2cea4a4b2337c71fcd2e1e66285f448e15
rishthak-wk <77300593+rishthak-wk@users.noreply.github.com>
2021-11-07 16:16:21 -0600

    fixed j orig

--- j_gen.m
+++ j_gen.m
@@ -5,11 +5,11 @@
 % beta - 
 % g_vector - 
 
-function j = j_gen(sigma_vec,alpha, beta, g_vector)
+function j = j_gen(delta_vec,alpha, beta, g_vec)
 %J_GEN defines the cost function
 
-toxic = sum(sigma_vec.^2);
-tumor1 = alpha*g_vector;
+toxic = sum(delta_vec.^2);
+tumor1 = alpha*g_vec(end);
 tumor2 = beta*trap_int(dt, g_vector);
 
 j = toxic + tumor1 + tumor2;
