1dce5dae6c148424d8cfe59efc59f8b3740f0845
BenjaminTan1 <78334905+BenjaminTan1@users.noreply.github.com>
2021-11-07 16:26:08 -0600

    Add files via upload
    Moved discretization to its own header file.

--- Project002_MATLAB.m
+++ Project002_MATLAB.m
@@ -29,24 +29,3 @@ for k = 0:10
     dt = 0.01 * T / 2^k;
     [f, g] = discretization(dt, g0, f0);
 end
-
-function [fSolve, gSolve] = discretization(h, g0, f0)
-
-    global p lambdaA lambdaK lambdaD;
-    
-    n = 50 / h;
-    
-    g = zeros(n,1);
-    f = zeros(n,1);
-    
-    g(1) = g0;
-    f(1) = f0;
-    
-    for i = 1:n
-        g(i+1) = g(i) + h * (lambdaD * g(i) * (1 - g(i)) - lambdaA * g(i) - lambdaK * g(i) * f(i));
-        f(i+1) = f(i) + h * (-1 * lambdaD * f(i) + p(i * h));
-    end
-    
-    fSolve = f;
-    gSolve = g;
-end
