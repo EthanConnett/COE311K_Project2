3c12e540653c2f5451af12cffbeb122280c38abe
rishthak-wk <77300593+rishthak-wk@users.noreply.github.com>
2021-11-07 16:10:02 -0600

    fixng sum for j orig

--- j_gen.m
+++ j_gen.m
@@ -1,12 +1,14 @@
 %% J_GEN
 % Inputs
 % sigma - number of drug inputs
-% 
+% alpha - 
+% beta - 
+% g_vector - 
 
 function j = j_gen(sigma_vec,alpha, beta, g_vector)
 %J_GEN defines the cost function
 
-toxic = sum(sigma_vec^2);
+toxic = sum(sigma_vec.^2);
 tumor1 = alpha*g_vector;
 tumor2 = beta*trap_int(dt, g_vector);
 
